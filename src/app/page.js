7ed1cbec661b030242a28bc9fe78859f83d495e8
MehediMehad <132126469+MehediMehad@users.noreply.github.com>
2024-06-19 13:55:58 +0600

    hero secsion

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
+import Hero from "@/components/Hero";
 import HomePage from "@/components/HomePage";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="">
-      <HomePage></HomePage>
+        {/* <HomePage></HomePage> */}
+        <Hero></Hero>
     </main>
   );
 }
