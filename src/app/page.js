5fbcc178b196aea04a2c22c39e33d889bd9bcb1e
MehediMehad <132126469+MehediMehad@users.noreply.github.com>
2024-06-19 19:16:24 +0600

    my skills secsion added

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,6 @@
 import Hero from "@/components/Hero";
 import HomePage from "@/components/HomePage";
+import Skills from "@/components/Skills";
 import Image from "next/image";
 
 export default function Home() {
@@ -7,6 +8,7 @@ export default function Home() {
     <main className="">
         {/* <HomePage></HomePage> */}
         <Hero></Hero>
+        <Skills></Skills>
     </main>
   );
 }
---
+++ src/components/Skills.jsx
@@ -0,0 +1,50 @@
+"use client"
+import React from "react";
+import { SiExpress, SiTailwindcss } from "react-icons/si";
+import { RiNextjsFill } from "react-icons/ri";
+
+import {
+  DiJavascript1,
+  DiReact,
+  DiNodejsSmall,
+  DiHtml5,
+  DiCss3,
+  DiMongodb
+} from 'react-icons/di'
+import { TypeAnimation } from "react-type-animation";
+const Skills = () => {
+  return (
+    <div className="max-w-[1200px] mx-auto p-10 md:p-0 ">
+      <div className="">
+
+        
+        <div className="text-3xl p-12 font-bold text-center glass pt-5">
+        <TypeAnimation className="mb-5"
+                            sequence={[
+                                "My Skills",
+                                1000,
+                                "My Skills",
+                                1000,
+                            ]}
+                            wrapper="span"
+                            speed={50}
+                            repeat={Infinity}
+                        />
+          <div className="flex justify-around gap-4 mt-5">
+            <DiHtml5 className="text-orange-600" />
+            <DiCss3 className="text-blue-600" />
+            <SiTailwindcss className="text-[#38bdf8]" /> 
+            <DiJavascript1 className="text-yellow-500" />
+            <DiReact className="text-blue-500" />
+            <RiNextjsFill className="text-black bg-white rounded-full border-0" />
+            <DiNodejsSmall className="text-green-500" />
+            <SiExpress className="text-white" />
+            <DiMongodb className="text-green-500" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Skills;
