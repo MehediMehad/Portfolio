ddfa09216ec02cdfacf4ab0622b88d929db55c95
MehediMehad <132126469+MehediMehad@users.noreply.github.com>
2024-07-05 19:46:19 +0600

    about me

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,10 +1,33 @@
 "use client"
+import myPic from '../../public/assets/myPic.jpg'
+
+import Image from "next/image";
 
 const About = () => {
     return (
-        <div className="">
-            
-        </div>
+        <>
+            <div className='max-w-[1200px] mx-auto my-36 glass p-5' id="about">
+            <h3 className="text-4xl font-semibold text-center text-[#d1d5db]">About <span>Me</span></h3>
+                <div className="grid grid-cols-2 ">
+                    {/* study */}
+                    <div className="col-span-2 md:col-span-1 w-full pb-5">
+                        <div className="p-5 text-[#d1d5db]">
+                            <p className='mt-4 active-project inline-block px-6 text-center mx-auto'>Educational</p>
+                            <p className='mt-5'>I am a dedicated and enthusiastic web developer studying Computer Science and Technology at GraphicArts Institute. </p>
+                        </div>
+                        <div className="p-5 text-[#d1d5db]">
+                            <p className='mt-4 active-project inline-block px-6 text-center mx-auto'>Educational</p>
+                            <p className='mt-5'>I am committed to continuous learning and improvement, constantly exploring new technologies and methodologies to enhance my skills. I am eager to pursue a job or internship to drive my web development career toward success. My goal is to contribute to innovative projects and deliver outstanding results. </p>
+                        </div>
+                    </div>
+                    {/* gol */}
+                    <div className="hidden md:block col-span-1 p-5 w-full bg-gray-700">
+                    <Image placeholder="blur" className="rounded-xl ml-28" src={myPic} alt='My Photo' height={490} />
+                    </div>
+                    
+                </div>
+            </div>
+        </>
     );
 };
 
--- src/components/SkillsSection/SkillsInfoCard.jsx
+++ src/components/SkillsSection/SkillsInfoCard.jsx
@@ -4,28 +4,7 @@ import Image from 'next/image';
 
 const SkillsInfoCard = ({ heading, skills }) => {
     return (
-        // <div className='skills-info-card'>
-        //     <h6>{heading}</h6>
-
-        //     <div className="skills-info-content ">
-        //         {skills.map((item, index) => (
-        //             <React.Fragment key={`skill_${index}`}>
-        //                 <div className="skill-info">
-        //                     {/* <p>{item.skill}</p> */}
-        //                     {/* <p className='percentage'>{item.percentage}</p> */}
-        //                     <Image className= 'text-white text-center items-center' src={item.img} alt={item.skill} width={60} height={60} />
-        //                 </div>
-
-        //                 <div className="skills-progress-bg">
-        //                     <div className="skills-progress" style={{ width: item.percentage }} />
-        //                     {/* <Image className= 'text-white text-center items-center' src={item.img} alt={item.skill} width={60} height={60} /> */}
-        //                 </div>
-        //             </React.Fragment>
-        //         ))}
-        //     </div>
-        // </div>
         <>
-                {/* <p>{heading}</p> */}
             <div className="grid grid-cols-3 justify-center text-center  items-center h-60">
                 {skills.map((item, index) => (
                     <React.Fragment key={`skill_${index}`}>
